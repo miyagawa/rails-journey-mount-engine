2066d82cda092f417c121c21857734ebef2452fb
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-07-02 15:08:06 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MountJourney::Application.config.secret_token = 'f1d72b27c21e1a3b7527100b420566998fcb146c843ae9a85ab7dc53ac16c36fbe11f46e58453faecc30fd37f47c7d5838bf83511af42d1fd200b7fbfb9c9764'
